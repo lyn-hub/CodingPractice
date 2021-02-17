60bb98be6f494b5924f5db9a22d18c1611bd0e8c
lyn-hub <linjingzhao0608@gmail.com>
2021-02-17 11:34:06 -0800

    practice

--- src/practice/com/Test.java
+++ src/practice/com/Test.java
@@ -21,7 +21,12 @@ public class Test {
 	}
 	public static void main(String[] args) {
         
-		
+//		String s1 = "1";
+//		String s2 = "2";
+//		String s3 = s1 + s2;
+//		StringBuilder  sb1 = new StringBuilder(s3);
+//	
+			
 		//string convert to int
 //		int r = Integer.parseInt("200");
 //		//string concatenation operator
@@ -31,19 +36,19 @@ public class Test {
 //		
 //		List<String> list = Arrays.asList("hot","dot","dog","lot","log","cog");	
 //		
-		TreeNode n1 = new TreeNode(5);
-		TreeNode n2 = new TreeNode(3);
-		TreeNode n3 = new TreeNode(8);
-		TreeNode n4 = new TreeNode(1);
-		TreeNode n5 = new TreeNode(4);
-		TreeNode n6 = new TreeNode(11);
-//		TreeNode n7 = new TreeNode(7);
-		n1.left =  n2;
-		n1.right = n3;
-		n2.left = n4;
-		n2.right = n5;
-	//	n3.left = n6;
-		n3.right = n6;
+//		TreeNode n1 = new TreeNode(5);
+//		TreeNode n2 = new TreeNode(3);
+//		TreeNode n3 = new TreeNode(8);
+//		TreeNode n4 = new TreeNode(1);
+//		TreeNode n5 = new TreeNode(4);
+//		TreeNode n6 = new TreeNode(11);
+////		TreeNode n7 = new TreeNode(7);
+//		n1.left =  n2;
+//		n1.right = n3;
+//		n2.left = n4;
+//		n2.right = n5;
+//	//	n3.left = n6;
+//		n3.right = n6;
 		
 //		List<String> list1 = new ArrayList<>();
 //		list1.add("a");
@@ -62,29 +67,29 @@ public class Test {
 		ListNode head = n1;
 		*/
 		
-		/*
-		PriorityQueue<TreeNode> minHeap = new PriorityQueue<>(new Comparator<TreeNode>() {
-			public int compare (TreeNode e1, TreeNode e2) {
-				return e2.key - e1.key;
-			}
-		});
-		minHeap.offer(n1);
-		minHeap.offer(n2);
-		minHeap.offer(n3);
-		minHeap.offer(n4);
 		
-		
-	//	String[] input = new String[] {"wrt","wrf","er","ett","rftt"};
-		char[][] gym = { { ' ', 'E', ' ' },
-
-				  {  ' ', ' ',  ' ' },
-
-				  {  ' ',  ' ', ' ' } };
-	//	int[] a = new int[] {4,3,3,1,5}; 
-	//	int[] b = new int[] {3,4,2};
+//		PriorityQueue<TreeNode> minHeap = new PriorityQueue<>(new Comparator<TreeNode>() {
+//			public int compare (TreeNode e1, TreeNode e2) {
+//				return e2.key - e1.key;
+//			}
+//		});
+//		minHeap.offer(n1);
+//		minHeap.offer(n2);
+//		minHeap.offer(n3);
+//		minHeap.offer(n4);
+//		
+//		
+//	//	String[] input = new String[] {"wrt","wrf","er","ett","rftt"};
+//		char[][] gym = { { ' ', 'E', ' ' },
+//
+//				  {  ' ', ' ',  ' ' },
+//
+//				  {  ' ',  ' ', ' ' } };
+		double[] a = new double[] {4,-2,-3,-2,3,-1,-2,6}; 
+		int[] b = new int[] {4,1,3,2};
 	//	int k = 4;
-        int[][] matrix = new int[][] {{0,0},{0,1},{1,2},{2,1}};
-        */
+    //  int[][] matrix = new int[][] {{0,0},{0,1},{1,2},{2,1}};
+        
 		
 		
 //	//	int[][] pre = {{3,1},{2,0},{1,2},{0,3}};
@@ -94,53 +99,37 @@ public class Test {
 //	//	double[][] matrix = new double[][] { {2,-1,0.5,1,-3},{0,-2,-1,2,0.1},{3,0.2,1,-3,-2}};
 		
 		Test solution = new Test();
-		List<Integer> result = solution.zigZag(n1);
+		int result = solution.minCost(b);
 		System.out.println(result);
 //	//	}
 		
  }
 
 	
-	public List<Integer> zigZag(TreeNode root) {
-	    // Write your solution here
-	    List<Integer> result = new ArrayList<>();
-	    if (root == null) {
-	      return result;
-	    }
-	    Deque<TreeNode> deque = new ArrayDeque<>();
-	    deque.offerFirst(root);
-	    int count = 0;
-	    while (!deque.isEmpty()) {
-	      int size = deque.size();
-	      for (int i = 0; i < size; i++) {
-	        if (count / 2 == 0) {
-	          //poll out
-	          TreeNode cur = deque.pollLast();
-	          result.add(cur.key);
-	          //offer
-	         if (cur.right != null) {
-	            deque.offerFirst(cur.right);
-	         }
-	          if (cur.left != null) {
-	           deque.offerFirst(cur.left);
-	         }        
-	       } else {
-	         //poll out 
-	          TreeNode cur = deque.pollFirst();
-	          result.add(cur.key);
-	          //offer
-	          if (cur.left != null) {
-	            deque.offerLast(cur.left);
-	          }
-	          if (cur.right != null) {
-	            deque.offerLast(cur.right);
-	         }
-	        }
-	      }
-	       count++;
-	    }
-	    return result;
-	  }
+	
+	public int minCost(int[] stones) {
+		// Write your solution here
+		int[][] M = new int[stones.length][stones.length];
+    for (int j = 0; j < M.length; j++) {
+      for (int i = j; i >= 0; i--) {
+        if (i == j) {
+          M[i][j] = 0;
+        } else if (i + 1 == j) {
+          M[i][j] = stones[i] + stones[j];
+        } else {
+          M[i][j] = Integer.MAX_VALUE;
+          for (int k = i + 1; k <= j; k++) {
+            int cost1 = M[i][k - 1] + M[k][j];
+            int temp1 = M[i][k - 1] == 0? stones[i] : M[i][k - 1];
+            int temp2 = M[k][j] == 0 ? stones[j] : M[k][j];
+            int cost2 = temp1 + temp2;
+            M[i][j] = Math.min(M[i][j], cost1 + cost2);
+          }
+        }
+      }
+    }  
+		return M[0][M.length - 1];
+	}
 }
 
 	
